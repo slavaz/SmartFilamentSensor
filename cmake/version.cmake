876a36ec85272087526e694ac749c64c27b38547
Slava Zanko <slavazanko@gmail.com>
2022-09-27 21:12:03 +0200

    Add auto versioning for git repo
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,1 +1,21 @@
-set(PROJECT_VERSION "1.0")
+
+execute_process(COMMAND git describe  --abbrev=0 --tags
+                OUTPUT_VARIABLE GIT_TAG
+                ERROR_QUIET)
+
+if(NOT GIT_TAG)
+    set(GIT_TAG "0.0")
+endif()
+
+execute_process(COMMAND git log --pretty=format:'%h' -n 1
+    OUTPUT_VARIABLE GIT_REV ERROR_QUIET)
+
+execute_process(COMMAND git rev-parse --abbrev-ref HEAD
+    OUTPUT_VARIABLE GIT_BRANCH ERROR_QUIET)
+
+string(STRIP "${GIT_TAG}" GIT_TAG)
+string(STRIP "${GIT_REV}" GIT_REV)
+string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
+string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+
+set(VERSION "${GIT_TAG}-${GIT_REV}-${GIT_BRANCH}")
